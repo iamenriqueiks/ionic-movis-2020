db7ace5afde23e619910adab00b477546d349e20
Enrique Díaz Romero <enriqued_93@hotmail.com>
2020-01-09 14:56:45 -0600

    Crear lista de cards con acciones

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,140 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AlertController} from '@ionic/angular';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+    cardsData = [
+        {
+            title: 'Hola equipo',
+            subtitle: 'El agua esta bien',
+            contentText: `Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Cupiditate,
+            nam quidem. Adipisci aliquam eaque hic nulla
+            suscipit. Assumenda atque consequuntur esse eum
+             maxime mollitia obcaecati repellendus vel veritatis
+             voluptate. Fugit.`
+        },
+        {
+            title: 'Hola equipo 2',
+            subtitle: 'El agua esta bien',
+            contentText: `Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Cupiditate,
+            nam quidem. Adipisci aliquam eaque hic nulla
+            suscipit. Assumenda atque consequuntur esse eum
+             maxime mollitia obcaecati repellendus vel veritatis
+             voluptate. Fugit.`
+        },
+        {
+            title: 'Hola equipo 3',
+            subtitle: 'El agua esta bien',
+            contentText: `Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Cupiditate,
+            nam quidem. Adipisci aliquam eaque hic nulla
+            suscipit. Assumenda atque consequuntur esse eum
+             maxime mollitia obcaecati repellendus vel veritatis
+             voluptate. Fugit.`
+        },
+        {
+            title: 'Hola equipo 3',
+            subtitle: 'El agua esta bien',
+            contentText: `Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Cupiditate,
+            nam quidem. Adipisci aliquam eaque hic nulla
+            suscipit. Assumenda atque consequuntur esse eum
+             maxime mollitia obcaecati repellendus vel veritatis
+             voluptate. Fugit.`
+        },
+        {
+            title: 'Hola equipo 3',
+            subtitle: 'El agua esta bien',
+            contentText: `Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Cupiditate,
+            nam quidem. Adipisci aliquam eaque hic nulla
+            suscipit. Assumenda atque consequuntur esse eum
+             maxime mollitia obcaecati repellendus vel veritatis
+             voluptate. Fugit.`
+        },
+        {
+            title: 'Hola equipo 3',
+            subtitle: 'El agua esta bien',
+            contentText: `Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Cupiditate,
+            nam quidem. Adipisci aliquam eaque hic nulla
+            suscipit. Assumenda atque consequuntur esse eum
+             maxime mollitia obcaecati repellendus vel veritatis
+             voluptate. Fugit.`
+        },
+        {
+            title: 'Hola equipo 3',
+            subtitle: 'El agua esta bien',
+            contentText: `Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Cupiditate,
+            nam quidem. Adipisci aliquam eaque hic nulla
+            suscipit. Assumenda atque consequuntur esse eum
+             maxime mollitia obcaecati repellendus vel veritatis
+             voluptate. Fugit.`
+        },
+        {
+            title: 'Hola equipo 3',
+            subtitle: 'El agua esta bien',
+            contentText: `Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Cupiditate,
+            nam quidem. Adipisci aliquam eaque hic nulla
+            suscipit. Assumenda atque consequuntur esse eum
+             maxime mollitia obcaecati repellendus vel veritatis
+             voluptate. Fugit.`
+        },
+        {
+            title: 'Hola equipo 3',
+            subtitle: 'El agua esta bien',
+            contentText: `Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Cupiditate,
+            nam quidem. Adipisci aliquam eaque hic nulla
+            suscipit. Assumenda atque consequuntur esse eum
+             maxime mollitia obcaecati repellendus vel veritatis
+             voluptate. Fugit.`
+        },
+        {
+            title: 'Hola equipo 3',
+            subtitle: 'El agua esta bien',
+            contentText: `Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Cupiditate,
+            nam quidem. Adipisci aliquam eaque hic nulla
+            suscipit. Assumenda atque consequuntur esse eum
+             maxime mollitia obcaecati repellendus vel veritatis
+             voluptate. Fugit.`
+        }
+    ];
 
+    constructor(private alertController: AlertController) {
+    }
+
+    onBeerClicked() {
+        console.log('quiero una no tan fria!');
+    }
+
+    openAlert() {
+        this.alertController.create({
+            header: 'Hello world!',
+            subHeader: '¡Los baños estan mas limpios ahora!',
+            buttons: [
+                {
+                    text: 'Cerrar',
+                    handler: () => {
+                        console.log('bye bye!');
+                    }
+                }
+            ]
+        }).then(alert => {
+            return alert.present();
+        }).then(() => {
+            console.log('ya se terminó de abrir 100% seguro');
+        });
+
+    }
 }
